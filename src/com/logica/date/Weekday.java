28a7623bb27ae476d1f30344191bce78977f9d8d
Jithu Gopal <jithug87@gmail.com>
2011-09-20 18:28:05 +0530

    Improved commit

--- src/com/logica/date/Weekday.java
+++ src/com/logica/date/Weekday.java
@@ -8,7 +8,6 @@ public class Weekday {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
 
 		if (dayOfWeek == Calendar.SUNDAY || dayOfWeek == Calendar.SATURDAY) {
